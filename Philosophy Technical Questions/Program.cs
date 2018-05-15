8639993c998501179e1a42e317c0352f628e3c53
Diaa Elkhateeb <32547282+d2msk@users.noreply.github.com>
2018-05-15 18:05:03 +0200

    First commit

--- Philosophy Technical Questions/Program.cs
+++ Philosophy Technical Questions/Program.cs
@@ -15,16 +15,10 @@ namespace Philosophy_Technical_Questions
             var x = new int[] { 9 };
             object y = new int[] { 10 };
 
-            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
             Console.WriteLine("i " + i);
-            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
             Console.WriteLine("j " + j);
-            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
             Console.WriteLine("x " + x[0]);
-            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
-            var q = y as int[];
-            Console.WriteLine(q[0]);
-            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
+            Console.WriteLine((y as int[])[0]);
 
             Console.ReadLine();
         }
