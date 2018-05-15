443fd44c6aedfad8fbcb1742e75a3e7c0d2305cc
Diaa Elkhateeb <32547282+d2msk@users.noreply.github.com>
2018-05-15 18:02:22 +0200

    Add project files.

---
+++ Philosophy Technical Questions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Philosophy_Technical_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 7;
+            int j = 8;
+            var x = new int[] { 9 };
+            object y = new int[] { 10 };
+
+            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
+            Console.WriteLine("i " + i);
+            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
+            Console.WriteLine("j " + j);
+            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
+            Console.WriteLine("x " + x[0]);
+            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
+            var q = y as int[];
+            Console.WriteLine(q[0]);
+            Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy:m:s:ms"));
+
+            Console.ReadLine();
+        }
+    }
+}
