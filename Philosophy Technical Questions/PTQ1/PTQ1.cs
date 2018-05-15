bee22fce6f27a4204d644c318adc73bd965ff1a0
Diaa Elkhateeb <diaamk@gmail.com>
2018-05-15 19:38:23 +0200

    Add z array.

--- Philosophy Technical Questions/PTQ1/PTQ1.cs
+++ Philosophy Technical Questions/PTQ1/PTQ1.cs
@@ -14,11 +14,13 @@ namespace Philosophy_Technical_Questions.PTQ1
             int j = 8;
             var x = new int[] { 9 };
             object y = new int[] { 10 };
+            int[] z = new int[] { 11 };
 
             Console.WriteLine("i " + i);
             Console.WriteLine("j " + j);
             Console.WriteLine("x " + x[0]);
             Console.WriteLine((y as int[])[0]);
+            Console.WriteLine(z[0]);
 
             Console.ReadLine();
         }
